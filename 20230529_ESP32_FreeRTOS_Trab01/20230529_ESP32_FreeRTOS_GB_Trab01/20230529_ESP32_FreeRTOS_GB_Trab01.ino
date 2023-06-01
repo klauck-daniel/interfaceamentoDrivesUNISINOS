8fce0657e155ca29d2c55d5123486810a0b205da
Daniel Klauck <daniel_klauck@hotmail.com>
2023-05-31 22:46:21 -0300

    inicio da task de tempo

--- 20230529_ESP32_FreeRTOS_Trab01/20230529_ESP32_FreeRTOS_GB_Trab01/20230529_ESP32_FreeRTOS_GB_Trab01.ino
+++ 20230529_ESP32_FreeRTOS_Trab01/20230529_ESP32_FreeRTOS_GB_Trab01/20230529_ESP32_FreeRTOS_GB_Trab01.ino
@@ -1,71 +1,142 @@
-/*CABEÇALHO*/
+/* ===========================================================
+
+   @Curso de Engenharia Elétrica e Engenharia da Computação
+   @Tarefa2GB-Cronômetro parcial com FreeRTOS 
+      
+   Disciplina de Interfaceamento e Drivers
+   
+   Professor: Lúcio Rene Prade
+   Autor: Daniel Klauck e Felipe Paloschi
+
+==============================================================*/
+
+//============================================================
+// --- Mapeamento de Hardware ---
+#define bt_start GPIO_NUM_21
+#define bt_stop GPIO_NUM_22
+#define bt_parcial1 GPIO_NUM_23
+#define bt_parcial2 GPIO_NUM_19
+
+bool aux_bt_start = 0,
+     aux_bt_stop = 0,
+     aux_bt_parcial1 = 0,
+     aux_bt_parcial2 = 0,
+     aux_iniciar = 0;
 
 
-//Variáveis globais de controle do cronometro
-byte start = 0;
-byte stop = 0;
-byte reset = 0;
+SemaphoreHandle_t xMutex1;
+SemaphoreHandle_t xMutex2;
 
-//Variáveis globais das parciais, com 3 posições cada
-char p1[3];
-char p2[3];
+void setup() 
+{
 
-//Variáveis globais do tempo
-int hora = 0;
-int minuto = 0;
-int segundo = 0;
+//==========================================
+  pinMode(bt_start, INPUT_PULLUP);                                                       
+  pinMode(bt_stop, INPUT_PULLUP);                                                       
+  pinMode(bt_parcial1, INPUT_PULLUP);
+  pinMode(bt_parcial2, INPUT_PULLUP);                                                       
 
-//Mutex para proteção das variáveis
-SemaphoreHandle_t xMutex01;
-SemaphoreHandle_t xMutex02;
-
-
-void task_tempo(void *pvParameters){
-    while (true){
-    xSemaphoreTake(xMutex01,portMAX_DELAY);
-    Serial.print("Task 1: ");
-    Serial.println(start);
-    start =  start > 1 ? start-1 : start+1;
-    delay(500);
-    xSemaphoreGive(xMutex01);
-    
-    }
-}
-
-void task_btn(void *pvParameters){
-    while (true){
-    xSemaphoreTake(xMutex01,portMAX_DELAY);
-    Serial.print("Task 2: ");
-    Serial.println(start);
-    start =  start > 1 ? start-1 : start+1;
-    delay(500);
-    xSemaphoreGive(xMutex01);
-    
-    }
-}
-
-void task_serial(void *pvParameters){
-    while (true){
-    xSemaphoreTake(xMutex01,portMAX_DELAY);
-    Serial.print("Task 2: ");
-    Serial.println(start);
-    start =  start > 1 ? start-1 : start+1;
-    delay(500);
-    xSemaphoreGive(xMutex01);
-    
-    }
-}
-
-void setup(){
   Serial.begin(115200);
-    xMutex01 = xSemaphoreCreateMutex();
-    if(xMutex01 != NULL){
-         xTaskCreatePinnedToCore(task_tempo, "Print1", 10000,NULL, 3, NULL,0);
-         xTaskCreatePinnedToCore(task_btn, "Print2", 10000,NULL, 3, NULL,0);
-         xTaskCreatePinnedToCore(task_serial, "Print2", 10000,NULL, 3, NULL,0);
-    }
+  delay(1000);
+
+  //Cria tarefa 1
+  xTaskCreate(
+              task_tempo,   //Tarefa
+              "task_tempo", //Nome da tarefa
+              10000,      //Tamanho da pilha
+              NULL,       //Parâmetro de entrada não passa nada
+              3,          //Prioridade
+              NULL        //Identificador da tarefa
+              );
+
+  //Cria tarefa 2
+  xTaskCreate(
+              task_btn,   //Tarefa
+              "task_btn", //Nome da tarefa
+              10000,      //Tamanho da pilha
+              NULL,       //Parâmetro de entrada não passa nada
+              2,          //Prioridade
+              NULL        //Identificador da tarefa
+              );
+  
+  
+  //Cria tarefa 3
+  xTaskCreate(
+              task_display,   //Tarefa
+              "task_display", //Nome da tarefa
+              10000,      //Tamanho da pilha
+              NULL,       //Parâmetro de entrada não passa nada
+              1,          //Prioridade
+              NULL        //Identificador da tarefa
+              );
+  
 }
 
-void loop(){
-  //limbo
+// the loop function runs over and over again forever
+void loop() 
+{
+  //Serial.println("void loop");
+  //delay(1000);
+}
+
+void task_tempo(void*parametro){
+  int segundo = 0,
+      minuto = 0,
+      hora = 0;
+
+  while(1)
+  {
+    
+    if(segundo < 60){
+      segundo++;
+    }else{
+      segundo = 0;
+      if (minuto < 60){
+        minuto++;
+      }else{
+        minuto = 0;
+        if (hora < 24){
+          hora++;
+        }else{
+          hora = 0;
+        }
+      }
+    }
+
+  }
+}
+
+void task_btn(void*parametro)
+{
+  while(1)
+  {
+      if(!digitalRead(bt_start))  aux_bt_start  = 1;                             
+      if(digitalRead(bt_start) && aux_bt_start)                                  
+      {
+        aux_bt_start = 0;                                                           
+        aux_iniciar = 1;                   
+      }
+
+    for(int i=0; i<10; i++)
+    {
+    Serial.print("Funcao 2: ");
+    Serial.println(i);
+    delay(1000);
+    }
+    Serial.println("Funcao 2 terminada");
+  }
+}
+
+void task_display(void*parametro)
+{
+  while(1)
+  {
+    for(int i=0; i<10; i++)
+    {
+    Serial.print("Funcao 3: ");
+    Serial.println(i);
+    delay(1000);
+    }
+    Serial.println("Funcao 3 terminada");
+  }
 }
