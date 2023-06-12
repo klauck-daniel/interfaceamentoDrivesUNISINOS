d8c1627dd1f6d0ea4f6c33287c874560bc96d457
Daniel Klauck <daniel_klauck@hotmail.com>
2023-06-12 20:17:47 -0300

    implementação

--- 20230605_ESP32_TrabFinal/Etapa 02 - Código/ESP32_TrabFinal/ESP32_TrabFinal.ino
+++ 20230605_ESP32_TrabFinal/Etapa 02 - Código/ESP32_TrabFinal/ESP32_TrabFinal.ino
@@ -9,6 +9,13 @@
 ==============================================================*/
 
 //============================================================
+
+// --- Bibliotecas ---
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 // --- Mapeamento de Hardware ---
 #define bt_pp GPIO_NUM_12
 #define bt_mm GPIO_NUM_14
@@ -25,8 +32,20 @@
 #define out_03 GPIO_NUM_33
 #define out_04 GPIO_NUM_32
 
-#define OLED GPIO_NUM_21 //definir corretamente
-#define OLED GPIO_NUM_22 //definir corretamente
+// --- Defines do Display ---
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
 
 volatile int hora = 0;
 volatile int minuto = 0;
