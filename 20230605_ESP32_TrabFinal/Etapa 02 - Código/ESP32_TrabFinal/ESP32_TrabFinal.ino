e5d23ff51e89493bf3af8538b5bb32e1d3735763
fpaloschi <135670998+fpaloschi@users.noreply.github.com>
2023-06-12 21:12:49 -0300

    Add - btn

--- 20230605_ESP32_TrabFinal/Etapa 02 - Código/ESP32_TrabFinal/ESP32_TrabFinal.ino
+++ 20230605_ESP32_TrabFinal/Etapa 02 - Código/ESP32_TrabFinal/ESP32_TrabFinal.ino
@@ -17,16 +17,21 @@
 #include <Adafruit_SSD1306.h>
 
 // --- Mapeamento de Hardware ---
+// --- Define entrada dos botões de escolha ---
 #define bt_pp GPIO_NUM_12
 #define bt_mm GPIO_NUM_14
 #define bt_ok GPIO_NUM_27
 
+// --- Define entradas digitais do PLC ---
 #define in_01 GPIO_NUM_35
 #define in_02 GPIO_NUM_34
 #define in_03 GPIO_NUM_15
 #define in_04 GPIO_NUM_4
+
+// --- Define entrada analógica do PLC ---
 #define a_in_04 GPIO_NUM_13
 
+// --- Define saídas digitais do PLC ---
 #define out_01 GPIO_NUM_26
 #define out_02 GPIO_NUM_25
 #define out_03 GPIO_NUM_33
@@ -44,74 +49,44 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-
-
-
-volatile int hora = 0;
-volatile int minuto = 0;
-volatile int segundo = 0;
-
-volatile int aux_iniciar = 0;
-volatile int aux_zerar = 0;
-volatile int aux_parcial = 0;
-
-volatile int h = 0;
-volatile int m = 0;
-volatile int s = 0;
-
-volatile int parcial1_h = 0;
-volatile int parcial1_m = 0;
-volatile int parcial1_s = 0;
-
-volatile int parcial2_h = 0;
-volatile int parcial2_m = 0;
-volatile int parcial2_s = 0;
-
 int aux_conta = 0;
 
-bool aux_bt_start   = 0,
-     aux_bt_stop    = 0,
-     aux_bt_zerar   = 0,
-     aux_bt_parcial = 0;
+bool aux_bt_pp     = 0,
+     aux_bt_mm     = 0,
+     aux_bt_ok     = 0;
 
 TickType_t xTimeBefore, xTotalTimeSuspended;
 
 SemaphoreHandle_t xMutex1;
 SemaphoreHandle_t xMutex2;
 
-void task_btn(void*parametro)
+void task_ihm_btn_display(void*parametro)
 {
   while(1)
   {
     xSemaphoreTake(xMutex1,portMAX_DELAY);
 
-      if(!digitalRead(bt_start))  aux_bt_start  = 1;                             
-      if(digitalRead(bt_start) && aux_bt_start)                                  
+      if(!digitalRead(bt_pp))  aux_bt_pp  = 1;                             
+      if(digitalRead(bt_pp) && aux_bt_pp)                                  
       {
-        aux_bt_start = 0;                                                           
-        aux_iniciar = 1;                   
+        aux_bt_pp = 0;                                                                             
       }
 
-      if(!digitalRead(bt_stop))  aux_bt_stop  = 1;                             
-      if(digitalRead(bt_stop) && aux_bt_stop)                                  
+      if(!digitalRead(bt_mm))  aux_bt_mm  = 1;                             
+      if(digitalRead(bt_mm) && aux_bt_mm)                                  
       {
-        aux_bt_stop = 0;                                                           
-        aux_iniciar = 0;                   
+        aux_bt_mm = 0;                                                                              
       }
 
-      if(!digitalRead(bt_zerar))  aux_bt_zerar  = 1;                             
-      if(digitalRead(bt_zerar) && aux_bt_zerar)                                  
+      if(!digitalRead(bt_ok))  aux_bt_ok  = 1;                             
+      if(digitalRead(bt_ok) && aux_bt_ok)                                  
       {
-        aux_bt_zerar = 0;                                                           
-        aux_zerar = 1;                   
+        aux_bt_ok = 0;                                                                              
       }
 
-      if(!digitalRead(bt_parcial))  aux_bt_parcial  = 1;                             
-      if(digitalRead(bt_parcial) && aux_bt_parcial)                                  
-      {
-        aux_bt_parcial = 0;                                                           
-        aux_parcial = 1;                   
-      }
+    String var = ":";
+    Serial.print("+...-...ok "); 
+    Serial.println("");
 
     xSemaphoreGive(xMutex1);
 
@@ -123,90 +98,6 @@ void task_tempo(void*parametro)
 {  
   while(1)
   {
-
-    xTotalTimeSuspended = xTaskGetTickCount() - xTimeBefore;
-    
-    if (xTotalTimeSuspended >= 10)
-          {
-		  			xTimeBefore = xTaskGetTickCount();
-
-		  			if (segundo < 60) 
-              {segundo++;}
-            else
-              {
-                segundo = 0;
-		  			    if (minuto < 60)
-                  {minuto++;}
-                else
-                  {
-                    minuto = 0;
-		  			        if (hora < 24) 
-                      {hora++;}
-                    else
-                      {hora = 0;}
-		  			      }
-		  			  }
-		  	  }
-
-    xSemaphoreTake(xMutex1,portMAX_DELAY);
-    xSemaphoreTake(xMutex2,portMAX_DELAY);
-
-    if (aux_iniciar)
-      {
-			  h=hora;
-			  m=minuto;
-			  s=segundo;
-		  }
-      
-    if (aux_zerar)
-      {
-			  hora = 0;
-			  minuto = 0;
-			  segundo = 0;
-
-			  h=0;
-			  m=0;
-			  s=0;
-
-			  parcial1_h = 0;
-			  parcial1_m = 0;
-			  parcial1_s = 0;
-
-			  parcial2_h = 0;
-			  parcial2_m = 0;
-			  parcial2_s = 0;
-
-			  aux_zerar = 0;
-		  }
-      
-    if(aux_parcial)
-      {
-			  aux_conta++;
-			  if (aux_conta == 1)
-			    {
-            parcial1_h = hora;
-			      parcial1_m = minuto;
-			      parcial1_s = segundo;
-
-		  	    aux_parcial = 0;
-			    }
-
-			  if (aux_conta == 2)
-			 		{
-			 			aux_conta=0;
-
-            parcial2_h = hora;
-			      parcial2_m = minuto;
-			      parcial2_s = segundo;
-
-			 		  aux_parcial = 0;
-			 			}
-		  }
-    
-    xSemaphoreGive(xMutex1);
-    xSemaphoreGive(xMutex2);
-
-    delay(500);
 
   }
 }
@@ -255,7 +146,7 @@ void setup()
   pinMode(out_02, OUTPUT);  
   pinMode(out_03, OUTPUT);  
   pinMode(out_04, OUTPUT);
-  
+
   Serial.begin(115200);
   delay(1000);
 
@@ -271,29 +162,6 @@ void setup()
               3,          //Prioridade
               NULL,
               0        //Identificador da tarefa
-              );
-
-  //Cria tarefa 2
-  xTaskCreatePinnedToCore(
-              task_btn,   //Tarefa
-              "task_btn", //Nome da tarefa
-              10000,      //Tamanho da pilha
-              NULL,       //Parâmetro de entrada não passa nada
-              2,          //Prioridade
-              NULL,
-              0        //Identificador da tarefa
-              );
-  
-  
-  //Cria tarefa 3
-  xTaskCreatePinnedToCore(
-              task_display,   //Tarefa
-              "task_display", //Nome da tarefa
-              10000,      //Tamanho da pilha
-              NULL,       //Parâmetro de entrada não passa nada
-              1,          //Prioridade
-              NULL,        //Identificador da tarefa
-              0
               );
   
 }
